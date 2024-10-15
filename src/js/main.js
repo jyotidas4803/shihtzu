56d8bb1db7338efe7d01ef8f3ac843def4a80ef6
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-10-15 14:09:55 +0000

    added some fun facts in js

--- src/js/main.js
+++ src/js/main.js
@@ -5,19 +5,32 @@ import Option from "./components/Option";
 
 const BASE_URL = `https://dog.ceo/api/`;
 
+// where the list of dog breeds will be populated.
 const breedListEl = document.querySelector("#data-breed-list");
+
+// This image element will display the fetched dog images.
 const imageEl = document.querySelector("img");
 
 // === MARK: Fetch
+
+Purpose: Fetches the list of all dog breeds from the API.
 function getDogsList() {
   return fetch(`${BASE_URL}breeds/list/all`)
+
+  // Converts the response to JSON.
     .then((res) => res.json())
+
+    // Extracts the message property from the JSON, which contains the breeds data.
     .then((data) => data.message)
     .catch((err) => console.error("error aagyi", err));
 }
 
 // TODO: Implement
+
+// Purpose: Fetches a random image for a specified dog breed.
 function getDogImage(breed) {
+
+  // {breed} is the selected dog breed.
   fetch(`${BASE_URL}breed/${breed}/images/random`)
     .then((res) => res.json())
     .then((data) => data.message);
