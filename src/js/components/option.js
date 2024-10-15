d073827321f647b03732f41152ffe6414cacbfc8
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-10-15 13:30:50 +0000

    utils

--- src/js/components/option.js
+++ src/js/components/option.js
@@ -1,3 +1,8 @@
-export default function(breed){
-    
+import { capitalize } from "../utils";
+
+export default function (breed) {
+  const option = document.createElement("option");
+  option.textContent = capitalize(breed);
+  option.value = breed;
+  return option;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,37 @@
-import option from "./components/option";
+import Option from "./components/Option";
 
 // https://dog.ceo/api/breed/affenpinscher/images/random
 // https://dog.ceo/api/breeds/list/all
 
-const BASE_URL = `https://dog.ceo/api/`
+const BASE_URL = `https://dog.ceo/api/`;
 
+const breedListEl = document.querySelector("#data-breed-list");
 const imageEl = document.querySelector("img");
-const breedListEl =document.querySelector("data-breed-list");
-// console.log(dataBreedList)
 
-// fetch function
-
-function getDogList(){
- fetch(`${BASE_URL}breeds/list/all`)
- .then((res) => res.json())
- .then((data) => data.message)
- .catch((err) => console.error("error",err))
+// === MARK: Fetch
+function getDogsList() {
+  return fetch(`${BASE_URL}breeds/list/all`)
+    .then((res) => res.json())
+    .then((data) => data.message)
+    .catch((err) => console.error("error aagyi", err));
 }
-getDogList();
 
-
-// TODO: this
-function getDogImage(){}
-
-
-function renderSelect(){
-getDogList().then((breedList) => {
-  for(let breed in breedList){
-    const option= document.createElement("option")
-  option.textContent= "breed"
-  breedListEl.appendChild(option)
-  }
-} );
+// TODO: Implement
+function getDogImage(breed) {
+  fetch(`${BASE_URL}breed/${breed}/images/random`)
+    .then((res) => res.json())
+    .then((data) => data.message);
 }
+
+// === MARK: Render
+function renderSelect() {
+  getDogsList().then((breedList) => {
+    for (let breed in breedList) {
+      breedListEl.appendChild(Option(breed));
+    }
+  });
+}
+
 renderSelect();
 
-  // const option= document.createElement("option")
-  // option.textContent= "some data"
-  // option.value = "some value"
-
-
-function renderImage(){}
-
-
-
-// async - function can handle operations that don’t happen instantly.  async allows the script to wait for the image without freezing the whole webpage
-
-// async function getImage() {
-
-//     // fetch is like sending a message asking for information.
-//     //   await tells the script to wait until it gets the response before moving on.
-
-//   const res = await fetch(
-//     `https://dog.ceo/api/breed/affenpinscher/images/random`
-//   );
-
-// //   After getting the response, converts that response into JSON (JavaScript Object Notation).
-//   const data = await res.json();
-//   console.log(data.message);
-
-//   imageEl.src = data.message;
-// }
-
-// getImage();
+function renderImage() {}
